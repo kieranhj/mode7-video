b6251de0c1b3e15db0e1ed14ea61808e700fa617
kieranhj <kieran@kconnell.co.uk>
2016-10-02 18:12:09 +0100

    Stop creating additional inf files
    Delete existing inf files before making new ones.

--- compress_mode7_stream.bat
+++ compress_mode7_stream.bat
@@ -2,4 +2,5 @@ REM Usage: compress_mode7_stream <short name/dir>
 @echo off
 bin\exomizer.exe raw -m 4096 -c %1\%1_beeb.bin -o %1\%1_beeb.bin.exo
 bin\split.exe --bytes=199680 --numeric-suffixes %1\%1_beeb.bin.exo %1\files\%1_beeb_
+del %1\files\*.inf
 FOR %%F IN (%1\files\*) DO echo $.%1	000000 000000 > %%F.inf
